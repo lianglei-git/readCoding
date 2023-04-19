09970a96e518ec6085c2690341b93eff9d45ee45
梁磊 <lei.liang@yizhun-ai.com>
2023-04-19 12:33:50 +0800

    🧄Readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# type-canvas3d
-The webgl to framework
+# For ReadCoding 🔧
+
+> You can see the effect this project  in "https://sparrowui.cn/coding/?cardCode";
+`PS: Please wait a moment`
+
+
+### ✨ Features
+- 📖 Read Pdf File.
+- 🦭 Run Coding.
+- 👀 The card code for SparrowUI.
+
+### ⚙️ TODO
+- Read Format
+    - [ ] md
+    - [x] pdf
+    - [ ] txt
+    - [ ] epub
+    - [ ] mobi
+    - [ ] azw
+    - [ ] other
+- Coding
+    - [x] Basic.
+    - [ ] Support C++.
+    - [ ] Support Save Code.
+- View
+    - [ ] I don't Know.
+
+
+### 🏃 Run Code
+1. `git clone ...`
+2. `pnpm install`
+3. `pnpm start`
+4. `open http://localhost:5173`
--- packages/ui/src/store/plugins/Load/SpuiTesting.ts
+++ packages/ui/src/store/plugins/Load/SpuiTesting.ts
@@ -17,21 +17,21 @@ class SpuiTesting {
         this.startup();
     }
 
-    startup(){
+    startup() {
         this.app.Setting.PersistableLoadProcess.value = 91;
         let reactCode = localStorage.getItem('cardCodeReact');
         let htmlCode = localStorage.getItem('cardCodeHtml');
-        let c1 = reactCode || reactCode!=='false' ? reactCode : false
+        let c1 = reactCode || reactCode !== 'false' ? reactCode : false
         let c2 = htmlCode || htmlCode !== 'false' ? htmlCode : false
-        if(c1 && c2) {
-            let modal= Spui.Modal.config({
+        if (c1 && c2) {
+            let modal = Spui.Modal.config({
                 visible: true,
                 footer: null,
                 title: '代码片段可选择执行',
-                closable:false,
-                onClose() {},
-                bodyhtml: 
-                `
+                closable: false,
+                onClose() { },
+                bodyhtml:
+                    `
                     <p style='display:flex;justify-content: center; height: 100px;transform: scale(1.3);align-items: center;'>
                     <sp-button2 type='primary' style='margin-right:30px' onclick="window.uiSelectCardCode('html')">Html</sp-button2>
                     <sp-button2 type='danger' onclick="window.uiSelectCardCode('react')">React</sp-button2>
@@ -40,26 +40,27 @@ class SpuiTesting {
             });
             window.uiSelectCardCode = (type: 'html' | 'react') => {
                 modal.show(false)
-                if(type == 'html') return this.writeHtmlDemo(c2);
-                if(type == 'react') return this.writeReactDemo(c1);
+                if (type == 'html') return this.writeHtmlDemo(c2);
+                if (type == 'react') return this.writeReactDemo(c1);
             }
             return '';
         }
 
-        if(c1) return this.writeReactDemo(c1);
-        if(c2) return this.writeHtmlDemo(c2);
-        Spui.Message.error('无代码可供执行！')
+        if (c1) return this.writeReactDemo(c1);
+        if (c2) return this.writeHtmlDemo(c2);
+        Spui.Message.error('无代码可供执行！使用默认Code')
+        this.writeReactDemo(false);
     }
 
     writeReactDemo(uiForCardCode) {
-        if(uiForCardCode) {
+        if (uiForCardCode) {
             uiForCardCode = JSON.parse(uiForCardCode);
         }
         this.app.TreeStore.startup(uiForCardCode ?? default_react_template);
         this.app.Setting.PersistableLoadProcess.value = 100;
         setTimeout(() => {
             Array.from(document.querySelectorAll('.rc-tree-node-content-wrapper')).forEach((element: any) => {
-                if(element.title == 'demo.jsx') {
+                if (element.title == 'demo.jsx') {
                     element.click();
                 }
             })
@@ -68,14 +69,14 @@ class SpuiTesting {
 
     writeHtmlDemo(uiForCardCode) {
         // let uiForCardCode = localStorage.getItem('cardCodeHtml');
-        if(uiForCardCode) {
+        if (uiForCardCode) {
             uiForCardCode = JSON.parse(uiForCardCode);
         }
         this.app.TreeStore.startup(uiForCardCode ?? default_react_template);
         this.app.Setting.PersistableLoadProcess.value = 100;
         setTimeout(() => {
             Array.from(document.querySelectorAll('.rc-tree-node-content-wrapper')).forEach((element: any) => {
-                if(element.title == 'index.html') {
+                if (element.title == 'index.html') {
                     element.click();
                 }
             })
