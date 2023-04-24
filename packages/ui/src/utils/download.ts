19715f47e62597d2d2b087d3d65296a6e96bca50
梁磊 <lei.liang@yizhun-ai.com>
2023-04-24 14:49:48 +0800

    .

--- packages/ui/src/utils/download.ts
+++ packages/ui/src/utils/download.ts
@@ -7,7 +7,6 @@ function getDownloadUri (data) {
 
 function saveDataFile (data, fileName) {
     try {
-      debugger
         const element = document.createElement('a');
         const uri = getDownloadUri(data);
         element.href = uri;
@@ -34,38 +33,38 @@ const Password = 'my-secret-password';
 function encrypt(text, password = Password) {  
   // 将文本转换为字节数组  
   var bytes = CryptoJS.AES.encrypt(text, password);  
-  
+  return bytes.toString();
   // 将字节数组转换为Base64编码字符串  
-  var base64 = btoa(bytes);  
+  // var base64 = btoa(bytes);  
   
-  // 将Base64编码字符串转换为字节数组  
-  var encryptedBytes = atob(base64);  
+  // // 将Base64编码字符串转换为字节数组  
+  // var encryptedBytes = atob(base64);  
   
   // 返回加密后的字节数组  
-  return encryptedBytes;  
+  // return encryptedBytes;  
 }  
   
 // 解密函数  
-function decrypt(encryptedBytes, password = Password) {  
+function decrypt(ciphertext, password = Password) {  
   // 将字节数组转换为Base64编码字符串  
-  var base64 = atob(encryptedBytes);  
+  // var base64 = atob(encryptedBytes);  
   
   // 将Base64编码字符串转换为字节数组  
-  var bytes = CryptoJS.AES.decrypt(base64, password);  
-  
+  var bytes = CryptoJS.AES.decrypt(ciphertext, password);  
+  const decryptedPlaintext = bytes.toString(CryptoJS.enc.Utf8);
   // 将字节数组转换为字符串  
-  var decryptedText = atob(bytes);  
+  // var decryptedText = atob(bytes);  
   
   // 返回解密后的字符串  
-  return decryptedText;  
+  return decryptedPlaintext;  
 }  
   
 // // 测试加密解密函数  
-// var encryptedText = encrypt('Hello, world!', password);  
-// var decryptedText = decrypt(encryptedText, password);  
+var encryptedText = encrypt('Hello, world!', Password);  
+var decryptedText = decrypt(encryptedText, Password);  
   
-// console.log('原始文本：', encryptedText);  
-// console.log('加密后的文本：', decryptedText);
+console.log('原始文本：', encryptedText);  
+console.log('加密后的文本：', decryptedText);
 
 
 
