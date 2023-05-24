b10323d1b2f7130e67b4c0d3b34e69767b8469d6
梁磊 <lei.liang@yizhun-ai.com>
2023-05-24 10:17:27 +0800

    merge spui dev

---
+++ packages/ui/template/spuiDev/index.tsx
@@ -0,0 +1,12 @@
+import '@sparrowend/ui-core/components/button-v2'
+import '@sparrowend/ui-core/components/button-v2/style'
+
+const App = () => {
+  return <sp-button2>可以的呢</sp-button2>
+};
+
+window.ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
